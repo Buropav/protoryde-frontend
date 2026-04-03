ba6a28c43c73e34f93b20e36e92ffa8837bc2d94
Narain_BK <bknarains@gmail.com>
2026-04-03 19:50:57 +0530

    feat: align contents in splash screen

--- app/(auth)/index.tsx
+++ app/(auth)/index.tsx
@@ -65,7 +65,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     flex: 1,
-    paddingBottom: 60,
+    paddingBottom: 0,
   },
   shieldIcon: {
     fontSize: 64,
