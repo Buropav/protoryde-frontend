b299e9f26d32f1338f7935e230b71764c7d215f9
Narain_BK <bknarains@gmail.com>
2026-04-17 18:23:16 +0530

    chore: change background color of logo

--- app/(auth)/phone-verification.tsx
+++ app/(auth)/phone-verification.tsx
@@ -165,7 +165,7 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     justifyContent: "center",
     alignItems: "center",
-    marginBottom: 16,
+    marginBottom: 10,
   },
   buttonDisabled: { 
     opacity: 0.5 
